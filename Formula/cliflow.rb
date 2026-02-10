c4a0d4f0ef8b802a45f4dab837526814a0f529a5
adnankoroth <adnan@pintu.co.id>
2026-02-10 11:29:12 +0530

    Update sha256 for v1.0.0 setup fix

--- Formula/cliflow.rb
+++ Formula/cliflow.rb
@@ -11,7 +11,7 @@ class Cliflow < Formula
   desc "IDE-style terminal autocompletion for 800+ CLI tools - offline, privacy-first"
   homepage "https://github.com/adnankoroth/cliflow"
   url "https://github.com/adnankoroth/cliflow/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b0df54b2da8281e2aac33dc6202cdb46b03d620d42d35a33ce3dcedd99fba480"
+  sha256 "6247851a3a35bcb465ca1c95bf626139e67e107becb7e5232e689899447a273d"
   license "MIT"
   head "https://github.com/adnankoroth/cliflow.git", branch: "main"
 
