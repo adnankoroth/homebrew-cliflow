f1a6b03673628ab5187cceeca410facc964556cf
adnankoroth <adnan@pintu.co.id>
2026-02-10 11:19:51 +0530

    Update sha256

--- Formula/cliflow.rb
+++ Formula/cliflow.rb
@@ -11,7 +11,7 @@ class Cliflow < Formula
   desc "IDE-style terminal autocompletion for 800+ CLI tools - offline, privacy-first"
   homepage "https://github.com/adnankoroth/cliflow"
   url "https://github.com/adnankoroth/cliflow/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "a161108c4445c29dc3bee307a3ed5e9f96a81dbfa7ac0af68a652e3995128e15"
+  sha256 "63807153a12f759a2efd8b799fbf88651de441b628d6ca106d95af72ed15fe2a"
   license "MIT"
   head "https://github.com/adnankoroth/cliflow.git", branch: "main"
 
